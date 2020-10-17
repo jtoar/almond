2a2e7634af823a21a157a50429976782bf5492fa
dom <dominic.saadi@hey.com>
2020-10-17 12:07:11 -0700

    add gotrue

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import GoTrue from 'gotrue-js'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -6,13 +8,18 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
-import cache from './cache.js'
+const goTrueClient = new GoTrue({
+  APIUrl: 'https://MYAPP.netlify.app/.netlify/identity',
+  setCookie: true,
+})
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider graphQLClientConfig={{ cache }}>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={goTrueClient} type="goTrue">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
