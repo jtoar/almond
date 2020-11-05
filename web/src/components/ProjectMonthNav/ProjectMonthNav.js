16a8f3018b26c931daddffd66d0ca0719e158177
dom <dominic.saadi@hey.com>
2020-11-05 00:39:34 -0800

    refactor(projectmonthnav): lift cell up

--- web/src/components/ProjectMonthNav/ProjectMonthNav.js
+++ web/src/components/ProjectMonthNav/ProjectMonthNav.js
@@ -1,12 +1,8 @@
-import { useMutation } from '@redwoodjs/web'
-import { navigate, routes } from '@redwoodjs/router'
 import { createMachine } from 'xstate'
-import { assign } from '@xstate/immer'
 import { useMachine } from '@xstate/react'
 
 import ProjectsCell from 'src/components/ProjectsCell'
 
-import { getCurrentMonth } from 'src/lib/date'
 import { useClickOutside, useKeyDown } from 'src/lib/hooks'
 
 const toggleMachine = createMachine({
@@ -52,230 +48,100 @@ const ProjectMonthNav = ({ project }) => {
       {/* Menu items */}
       {current.matches('visible') ? (
         <div className="absolute z-10 top-6 left-2">
-          <ProjectMenu project={project}>
-            <CreateProject closeNav={() => send('HIDE')} />
-          </ProjectMenu>
+          <ProjectsCell currentProject={project} />
         </div>
       ) : null}
     </div>
   )
 }
 
-const UPDATE_PROJECT_NAME_BY_NAME = gql`
-  mutation UpdateProjectNameByName($name: String!, $newName: String!) {
-    updateProjectNameByName(name: $name, newName: $newName) {
-      id
-      updatedAt
-      name
-    }
-  }
-`
-
-const DELETE_PROJECT_BY_NAME = gql`
-  mutation DeleteProjectByName($name: String!) {
-    deleteProjectByName(name: $name) {
-      id
-    }
-  }
-`
-
-const useDeleteProjectByName = (project) => {
-  const [deleteProjectByName] = useMutation(DELETE_PROJECT_BY_NAME, {
-    variables: {
-      name: project,
-    },
-  })
-
-  return deleteProjectByName
-}
-
-const projectMenuMachine = (project) =>
-  createMachine({
-    id: 'projectMenu',
-    // We're typing as soon as this opens
-    initial: 'active',
-    context: {
-      project,
-      value: project,
-    },
-    states: {
-      active: {
-        on: {
-          CHANGE: {
-            actions: assign((context, event) => {
-              context.value = event.value
-            }),
-          },
-          // This has to be better...
-          UPDATE: {
-            actions: ['updateProject', 'navigateUpdate'],
-          },
-          // Keep the nav open here...
-          DELETE: {
-            actions: ['deleteProjectByName', 'navigateDelete'],
-          },
-        },
-      },
-    },
-  })
-
-const ProjectMenu = ({ project, children }) => {
-  const [updateProject] = useMutation(UPDATE_PROJECT_NAME_BY_NAME)
-  const deleteProjectByName = useDeleteProjectByName(project)
-
-  const [current, send] = useMachine(projectMenuMachine(project), {
-    actions: {
-      updateProject: (context) => {
-        console.log(context.project, context.value)
-        updateProject({
-          variables: {
-            name: context.project,
-            newName: context.value,
-          },
-        })
-      },
-      navigateUpdate: (context) => {
-        navigate(
-          routes.projectMonth({
-            project: context.value,
-            month: getCurrentMonth(),
-          })
-        )
-      },
-      deleteProjectByName,
-      navigateDelete: () => {
-        navigate(
-          routes.projectMonth({
-            project: document.querySelector('a').textContent,
-            month: getCurrentMonth(),
-          })
-        )
-      },
-    },
-  })
-
-  const handleBlur = () => {
-    if (current.context.value && current.context.value != project) {
-      send('UPDATE')
-    }
-  }
-
-  return (
-    <div className="w-48 bg-gray-50 border border-gray-900 rounded shadow-br divide-y divide-gray-900">
-      {/* rename, remove project */}
-      <div className="px-1 py-2">
-        <div className="flex space-x-1">
-          <input
-            type="text"
-            value={current.context.value}
-            onChange={({ target: { value } }) => send('CHANGE', { value })}
-            className="w-full px-2 py-1 focus:bg-gray-200 focus:shadow-br-inset rounded focus:outline-none"
-            onBlur={handleBlur}
-          />
-          <button
-            className="flex-shrink-0 border border-gray-900 rounded px-2"
-            onClick={() => send('DELETE')}
-          >
-            remove -
-          </button>
-        </div>
-      </div>
-      <div className="px-1 py-2">{children}</div>
-      {/* choose project */}
-      <div className="px-1 py-2">
-        <ProjectsCell selected={project} />
-      </div>
-    </div>
-  )
-}
-
-const CREATE_PROJECT = gql`
-  mutation CreateProjectMutation($name: String!) {
-    createProject(name: $name) {
-      id
-      createdAt
-      updatedAt
-      name
-    }
-  }
-`
-
-const createProjectMachine = createMachine(
-  {
-    id: 'createProject',
-    initial: 'typing',
-    context: {
-      value: '',
-    },
-    states: {
-      typing: {
-        on: {
-          CHANGE: {
-            actions: ['assignToValue'],
-          },
-          CREATE: {
-            actions: ['createProjectFromValue', 'navigateToValue', 'closeNav'],
-          },
-        },
-      },
-    },
-  },
-  {
-    actions: {
-      assignToValue: assign((context, event) => {
-        context.value = event.value
-      }),
-      navigateToValue: ({ value }) =>
-        navigate(
-          routes.projectMonth({
-            project: value,
-            month: getCurrentMonth(),
-          })
-        ),
-    },
-  }
-)
-
-const CreateProject = ({ closeNav }) => {
-  const [createProject] = useMutation(CREATE_PROJECT)
-  const [current, send] = useMachine(createProjectMachine, {
-    actions: {
-      createProjectFromValue: (context) => {
-        createProject({ variables: { name: context.value } })
-      },
-      closeNav,
-    },
-  })
-
-  const handleKeyDown = (e) => {
-    switch (e.key) {
-      case 'Enter':
-        if (e.ctrlKey) {
-          send('CREATE')
-        }
-        break
-    }
-  }
-
-  return (
-    <div className="flex space-x-1">
-      <input
-        type="text"
-        placeholder="project"
-        value={current.context.value}
-        onChange={({ target: { value } }) => send('CHANGE', { value })}
-        autoFocus={true}
-        className="w-full px-2 py-1 focus:bg-gray-200 focus:shadow-br-inset rounded focus:outline-none"
-        onKeyDown={handleKeyDown}
-      />
-      <button
-        className="flex-shrink-0 border border-gray-900 rounded px-2"
-        onClick={() => send('CREATE')}
-      >
-        add +
-      </button>
-    </div>
-  )
-}
-
 export default ProjectMonthNav
+
+// const CREATE_PROJECT = gql`
+//   mutation CreateProjectMutation($name: String!) {
+//     createProject(name: $name) {
+//       id
+//       createdAt
+//       updatedAt
+//       name
+//     }
+//   }
+// `
+
+// const createProjectMachine = createMachine(
+//   {
+//     id: 'createProject',
+//     initial: 'typing',
+//     context: {
+//       value: '',
+//     },
+//     states: {
+//       typing: {
+//         on: {
+//           CHANGE: {
+//             actions: ['assignToValue'],
+//           },
+//           CREATE: {
+//             actions: ['createProjectFromValue', 'navigateToValue', 'closeNav'],
+//           },
+//         },
+//       },
+//     },
+//   },
+//   {
+//     actions: {
+//       assignToValue: assign((context, event) => {
+//         context.value = event.value
+//       }),
+//       navigateToValue: ({ value }) =>
+//         navigate(
+//           routes.projectMonth({
+//             project: value,
+//             month: getCurrentMonth(),
+//           })
+//         ),
+//     },
+//   }
+// )
+
+// const CreateProject = ({ closeNav }) => {
+//   const [createProject] = useMutation(CREATE_PROJECT)
+//   const [current, send] = useMachine(createProjectMachine, {
+//     actions: {
+//       createProjectFromValue: (context) => {
+//         createProject({ variables: { name: context.value } })
+//       },
+//       closeNav,
+//     },
+//   })
+
+//   const handleKeyDown = (e) => {
+//     switch (e.key) {
+//       case 'Enter':
+//         if (e.ctrlKey) {
+//           send('CREATE')
+//         }
+//         break
+//     }
+//   }
+
+//   return (
+//     <div className="flex space-x-1">
+//       <input
+//         type="text"
+//         placeholder="project"
+//         value={current.context.value}
+//         onChange={({ target: { value } }) => send('CHANGE', { value })}
+//         autoFocus={true}
+//         className="w-full px-2 py-1 focus:bg-gray-200 focus:shadow-br-inset rounded focus:outline-none"
+//         onKeyDown={handleKeyDown}
+//       />
+//       <button
+//         className="flex-shrink-0 border border-gray-900 rounded px-2"
+//         onClick={() => send('CREATE')}
+//       >
+//         add +
+//       </button>
+//     </div>
+//   )
+// }
--- web/src/components/ProjectsCell/ProjectsCell.js
+++ web/src/components/ProjectsCell/ProjectsCell.js
@@ -1,5 +1,8 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
+import { useApolloClient } from '@redwoodjs/web'
+import { useMachine } from '@xstate/react'
 import { getCurrentMonth } from 'src/lib/date'
+import { createMachine, assign } from 'xstate'
 
 export const QUERY = gql`
   query ProjectsQuery {
@@ -18,7 +21,144 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ projects, selected }) => {
+const updateProject = ({ client, name, newName }) => {
+  return client.mutate({
+    mutation: gql`
+      mutation UpdateProjectNameByName($name: String!, $newName: String!) {
+        updateProjectNameByName(name: $name, newName: $newName) {
+          id
+          name
+        }
+      }
+    `,
+    variables: {
+      name,
+      newName,
+    },
+  })
+}
+
+const useProjectMachine = ({ projects, currentProject }) => {
+  const [state, send] = useMachine(
+    projectMachine({ projects, currentProject, client: useApolloClient() })
+  )
+  return [state, send]
+}
+
+const projectMachine = ({ projects, currentProject, client }) =>
+  createMachine(
+    {
+      id: 'projects',
+      context: {
+        projects,
+        currentProject,
+        client,
+        value: currentProject,
+      },
+      entry: assign({
+        currentProject: (context) =>
+          context.projects.find((project) => project.name === currentProject),
+      }),
+      initial: 'active',
+      states: {
+        active: {
+          on: {
+            CHANGE: {
+              actions: 'updateValue',
+            },
+            UPDATE: 'updating',
+            DELETE: {
+              actions: 'deleteProjectByName',
+            },
+          },
+        },
+        updating: {
+          invoke: {
+            id: 'updateProject',
+            src: ({ client, currentProject: { name }, value: newName }) =>
+              updateProject({
+                client,
+                name,
+                newName,
+              }),
+            onDone: {
+              target: 'active',
+              actions: [
+                assign({
+                  currentProject: ({ currentProject, value }) => {
+                    return {
+                      ...currentProject,
+                      name: value,
+                    }
+                  },
+                }),
+                (context) =>
+                  navigate(
+                    routes.projectMonth({
+                      project: context.value,
+                      month: getCurrentMonth(),
+                    })
+                  ),
+              ],
+            },
+          },
+        },
+      },
+    },
+    {
+      actions: {
+        updateValue: assign({
+          value: (_, event) => event.value,
+          projects: (context, event) => {
+            const projects = [...context.projects]
+
+            projects.splice(
+              projects.findIndex(
+                (project) => project.id === context.currentProject.id
+              ),
+              1,
+              {
+                ...context.currentProject,
+                name: event.value,
+              }
+            )
+
+            return projects
+          },
+        }),
+        deleteProjectByName: (context) => {
+          context.client.mutate({
+            mutation: gql`
+              mutation DeleteProjectByName($name: String!) {
+                deleteProjectByName(name: $name) {
+                  id
+                }
+              }
+            `,
+            variables: {
+              name: context.project,
+            },
+          })
+        },
+      },
+    }
+  )
+
+export const Success = ({ projects, currentProject, children }) => {
+  const [state, send] = useProjectMachine({
+    projects,
+    currentProject,
+  })
+
+  const handleBlur = () => {
+    if (
+      state.context.value &&
+      state.context.value != state.context.currentProject.name
+    ) {
+      send('UPDATE')
+    }
+  }
+
   const handleKeyDown = (e) => {
     switch (e.key) {
       case 'j':
@@ -32,8 +172,8 @@ export const Success = ({ projects, selected }) => {
     }
   }
 
-  const projectMonthPageLinks = projects.map((project) => {
-    const isSelected = project.name === selected
+  const projectMonthPageLinks = state.context.projects.map((project) => {
+    const isSelected = project.name === state.context.currentProject.name
     return (
       <Link
         key={project.id}
@@ -54,5 +194,31 @@ export const Success = ({ projects, selected }) => {
     )
   })
 
-  return <div className="flex flex-col">{projectMonthPageLinks}</div>
+  return (
+    <div className="w-48 bg-gray-50 border border-gray-900 rounded shadow-br divide-y divide-gray-900">
+      {/* rename, remove project */}
+      <div className="px-1 py-2">
+        <div className="flex space-x-1">
+          <input
+            type="text"
+            value={state.context.value}
+            onChange={({ target: { value } }) => send('CHANGE', { value })}
+            className="w-full px-2 py-1 focus:bg-gray-200 focus:shadow-br-inset rounded focus:outline-none"
+            onBlur={handleBlur}
+          />
+          <button
+            className="flex-shrink-0 border border-gray-900 rounded px-2"
+            onClick={() => send('DELETE')}
+          >
+            remove -
+          </button>
+        </div>
+      </div>
+      <div className="px-1 py-2">{children}</div>
+      {/* choose project */}
+      <div className="px-1 py-2">
+        <div className="flex flex-col">{projectMonthPageLinks}</div>
+      </div>
+    </div>
+  )
 }
